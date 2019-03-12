ebb4a2af721c045a4edf1d3e7571dd245503cba2
Steven Klein <steven.p.klein@gmail.com>
2019-03-12 10:49:37 -0500

    update all dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,59 @@
-var POSTCSS = {
-    "input": "src/css/app.css",
-    "output": "build/css/app.css",
-    "use": [
-        "postcss-import",
-        "postcss-cssnext",
-        "postcss-browser-reporter",
-        "postcss-reporter"
-    ],
+const path = require("path")
+
+module.exports = ({env}) => ({
+  "map": (env !== "production") ? true : false,
+  "plugins": {
     "postcss-import": {
-        path: [
-            "src/css"
-        ]
+      "path": [
+        `${process.env.npm_package_config_paths_src}/css`
+      ]
     },
-    "cssnano": {
-        safe: true,
-        autoprefixer: false //handled by postcss-cssnext
+    "postcss-apply": true,
+    "postcss-preset-env": {
+      "stage": 2,
+      "features": {
+        "custom-media-queries": true,
+        "color-mod-function": {
+          "unresolved": "error"
+        },
+        "custom-properties": {
+          "preserve": false
+        },
+        "custom-selectors": true,
+        "nesting-rules": true,
+      }
     },
-    "local-plugins": true,
-    "map": "file"
-}
-
-if(process.env.NODE_ENV === "production"){
-    POSTCSS.use.push('cssnano'); // add cssnano in production
-    POSTCSS.map = false; //sourcemaps to external file
-}
-
-module.exports = POSTCSS;
+    "postcss-url": [
+      {
+        filter: '**/node_modules/**', // node_module assets
+        url: "inline",
+        ignoreFragmentWarning: true,
+        maxSize: 1, // KB
+        fallback: "copy",
+        assetsPath: path.resolve(process.cwd(), `${process.env.npm_package_config_paths_dest}/vendor`),
+        useHash: true,
+        hashOptions: {
+          append: true
+        },
+      },
+      {
+        filter: `**/${process.env.npm_package_config_paths_src}/**`, // catch everything else from project.
+        url: "inline",
+        maxSize: 1, // KB
+        fallback: "copy",
+        assetsPath: path.resolve(process.cwd(), `${process.env.npm_package_config_paths_dest}/local`),
+        useHash: true,
+        hashOptions: {
+          append: true
+        },
+        basePath: path.resolve(process.cwd(), `${process.env.npm_package_config_paths_src}`)
+      },
+    ],
+    "cssnano": (env === "production") ? {
+      "safe": true,
+      "autoprefixer": false // handled by postcss-preset-env
+    } : false,
+    "postcss-browser-reporter": (env !== "production") ? true : false,
+    "postcss-reporter": (env !== "production") ? true : false
+  }
+})
--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,43 @@
-import buble from 'rollup-plugin-buble'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import uglify from 'rollup-plugin-uglify'
-import replace from 'rollup-plugin-replace'
-import includePaths from 'rollup-plugin-includepaths'
+import buble from "rollup-plugin-buble"
+import commonjs from "rollup-plugin-commonjs"
+import resolve from "rollup-plugin-node-resolve"
+import {uglify} from "rollup-plugin-uglify"
+import replace from "rollup-plugin-replace"
+import includePaths from "rollup-plugin-includepaths"
 
 export default {
-    entry: 'src/js/app.js',
-    dest: 'build/js/app.js',
-    format: 'iife',
-    useStrict: true,
-    plugins: [
-        includePaths({
-            include: {},
-            paths: ['src/js'],
-            external: [],
-            extensions: ['.js', '.json']
-        }),
-        resolve({
-            jsnext: true, // Default: false
-            main: true, // Default: true
-            browser: true,
-            extensions: ['.js', '.json']
-        }),
-        commonjs({
-            include: ['node_modules/**', 'resources/**'],
-            extensions: ['.js', '.json'],
-            // if false then skip sourceMap generation for CommonJS modules
-            sourceMap: true, // Default: true
-        }),
-        buble(),
-        replace({
-            exclude: 'node_modules/**',
-            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || 'development'),
-        }),
-        (process.env.NODE_ENV === 'production' && uglify())
-    ],
-    sourceMap: (process.env.NODE_ENV !== 'production') ? true : false
+  external: ["jquery"],
+  plugins: [
+    includePaths({
+      include: {},
+      paths: [`${process.env.npm_package_config_paths_src}/js`],
+      external: [],
+      extensions: [".js", ".json"]
+    }),
+    resolve({
+      jsnext: true, // Default: false
+      main: true, // Default: true
+      browser: true,
+      extensions: [".js", ".json"]
+    }),
+    commonjs({
+      include: ["node_modules/**", `${process.env.npm_package_config_paths_src}/**`],
+      extensions: [".js", ".json"],
+      // if false then skip sourceMap generation for CommonJS modules
+      sourceMap: true, // Default: true
+    }),
+    buble(),
+    replace({
+      exclude: "node_modules/**",
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development"),
+    }),
+    (process.env.NODE_ENV === "production" && uglify())
+  ],
+  output: {
+    format: "iife",
+    globals: {
+      jquery: "$"
+    },
+    sourcemap: (process.env.NODE_ENV !== "production") ? true : false
+  }
 }
